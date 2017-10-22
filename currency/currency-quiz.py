75556e7dbccf16dcce533830a91ac143b6035826
Michelia <michelia.brv@gmail.com>
2017-10-22 09:42:59 -0700

    i used float numbers

--- currency/currency-quiz.py
+++ currency/currency-quiz.py
@@ -5,8 +5,8 @@ def talk(words):
     system('say {0}'.format(words))
 
 
-def get_float(max_number):
-    float_number = uniform(0, max_number)
+def get_float(min_number, max_number):
+    float_number = uniform(min_number, max_number)
 
     #print float number below
     print('Before rounding : {0} '.format(float_number))
@@ -19,4 +19,14 @@ def get_float(max_number):
 
     return float_number
 
-get_float(max_number=10)
+#get_float(max_number=10)
+operator = '+'
+questions_count = 0
+if operator == '+':
+    top_number = get_float(3,50)
+    bottom_number = get_float(2,10)
+    correct_answer = top_number + bottom_number
+
+print_question = '{0}:  {1} {2} {3} = ?'.\
+                format(questions_count, top_number, operator, bottom_number)
+print(print_question)
