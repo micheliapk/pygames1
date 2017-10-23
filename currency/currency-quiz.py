e846893e861f5b6a1b37d84443cd3a228f28d3d1
Michelia <michelia.brv@gmail.com>
2017-10-22 19:18:09 -0700

    i checked correct answer

--- currency/currency-quiz.py
+++ currency/currency-quiz.py
@@ -23,10 +23,23 @@ def get_float(min_number, max_number):
 operator = '+'
 questions_count = 0
 if operator == '+':
-    top_number = get_float(3,50)
-    bottom_number = get_float(2,10)
+    top_number = get_float(4,8)
+    bottom_number = get_float(2,4)
     correct_answer = top_number + bottom_number
 
 print_question = '{0}:  {1} {2} {3} = ?'.\
                 format(questions_count, top_number, operator, bottom_number)
 print(print_question)
+input_string = input(': ')
+try:
+    student_answer = float(input_string)
+except:
+    talk('Do not enter junk values. Enter a valid number')
+print('student_answer is {0}'.format (student_answer))
+
+if correct_answer == student_answer:
+    print('You are correct')
+else:
+    print('You are wrong. The correct answer is {0}'.format(correct_answer))
+
+
