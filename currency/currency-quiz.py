c820cbff92a3fea37a4371eba1db471c870adeeb
Michelia <michelia.brv@gmail.com>
2017-10-28 16:13:59 -0700

    i fixed a bug by rounding correct_answer and added a dollar sign

--- currency/currency-quiz.py
+++ currency/currency-quiz.py
@@ -26,11 +26,14 @@ if operator == '+':
     top_number = get_float(4,8)
     bottom_number = get_float(2,4)
     correct_answer = top_number + bottom_number
+    correct_answer = round(correct_answer, 2)
 
-print_question = '{0}:  {1} {2} {3} = ?'.\
+print_question = '{0}:   ${1} {2} ${3} = ?'.\
                 format(questions_count, top_number, operator, bottom_number)
 print(print_question)
-input_string = input(': ')
+
+#TODO: talk_currency(5.67, 2.2, '+') ==> ('what is 5 dollars and 67 cents plus 2 dollars and 2 cents ')
+input_string = input(': $ ')
 try:
     student_answer = float(input_string)
 except:
@@ -41,5 +44,4 @@ if correct_answer == student_answer:
     print('You are correct')
 else:
     print('You are wrong. The correct answer is {0}'.format(correct_answer))
-
 
