f45f6e6b2602928d20c4c758f1642e9b5a7c91f3
Michelia <michelia.brv@gmail.com>
2018-11-06 20:18:43 -0800

    i used randint to ask random words

--- learn/test-list-of-dictionaries.py
+++ learn/test-list-of-dictionaries.py
@@ -1,24 +1,20 @@
-from os import system
-from random import randint
+#michelia's python playground****************
+colors = ['navy','purple','aqua','lavender']
 
-from datetime import datetime
+print (colors[1]) #question 3
 
+#question 4
+colors[0] = 'pink'
 
-def get_current_time():
-    return datetime.now()
+#question 5
+del colors[2]
 
-
-def is_5_minutes_done(start_time):
-    current_time = get_current_time()
-    return ((current_time - start_time).total_seconds()) / 60 >= 5
-
-
-def get_percent_score(questions_count, score):
-    return (score * 100) / questions_count
-
-
-def talk(words):
-    system('say {0}'.format(words))
+#question 6
+colors.append('red')
+print (colors)
+#question 7
+colors.insert(0, 'blue')
+print(colors)
 
 spell_list= [
     {
@@ -594,23 +590,21 @@ spell_list= [
 
 ]
 
+word_num_string = input('please enter a number between 0 to 60: ')
+word_num = int(word_num_string)
+
+print(spell_list[word_num])
 
 
 
-print('**** Welcome to Michelia\'s Spelling Beast!!!')
-talk('Welcome to Mikaylias Spelling Beast!')
-secret_word_number = randint(0, len(spell_list)-1)
-secret_word = spell_list[secret_word_number]
 
-print(secret_word['word'])
 
-talk('How do you spell the word ending ?')
-word_string = input()
-if word_string  == 'ending':
-    print('You are correct')
-    talk('You are correct')
-else:
-    print('You are wrong. This word is spelled like E N D I N G' )
-    talk('You are wrong. This word is spelled like E N D I N G')
+
+
+
+
+
+
+
 
 
